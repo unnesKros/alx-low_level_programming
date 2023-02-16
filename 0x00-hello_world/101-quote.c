d3b04d0e2ea4e796c16b63fac22e9139c68a2d56
unnesKros <younesraf16@gmail.com>
2023-02-16 06:51:34 -0800

    last question

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,6 +9,7 @@
  */
 int main(void)
 {
-write(2,"That piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+int s = strlen("That piece of art is useful\" - Dora Korpar, 2015-10-19\n");  
+write(2,"That piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
 return (1);
 }
