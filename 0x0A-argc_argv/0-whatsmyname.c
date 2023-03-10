885943ba10bc075c7ce2d2e840beeeadf380a654
unnesKros <younesraf16@gmail.com>
2023-03-10 12:47:08 -0800

    all tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+
 /**
-  * main - Prints its name
-  * @argc: argument count
-  * @argv: arguments
-  *
-  * Return: 0
-  */
+ * main - print the name of the file followed by a new line
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	(void) argc;
+
 	printf("%s\n", argv[0]);
 
 	return (0);
