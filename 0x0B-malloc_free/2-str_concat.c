28b67d057d90079d70c7a99c45886fcda59144ef
unnesKros <younesraf16@gmail.com>
2023-03-13 07:23:26 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,52 +2,38 @@
 #include <stdlib.h>
 
 /**
- * str_concat - function that concatenate tow strings
+ * str_concat - concatenate two strings using malloc
  * @s1: string 1
  * @s2: string 2
- * Return: the full string
+ * Return: pointer to concat string
  */
-
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, x;
-	char *ar;
+	char *a;
+	int i, j, c, d;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	ar = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
-
-	if (ar == NULL)
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
 		return (NULL);
 
-	if (s1 == NULL)
+	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
-		x = 0;
-		ar[0] = "";
-		x++;
-	}
-	else
-	{
-		for (x = 0; x < i; x++)
-			ar[x] = s1[x];
+		if (c < i)
+			a[c] = s1[c];
+		else
+			a[c] = s2[d++];
 	}
 
-	if (s2 == NULL)
-	{
-		ar[x] = "";
-		x++;
-	}
-	else
-	{
-
-		for (x = x; x < i + j; x++)
-			ar[x] = s2[x - i];
-	}
-
-	ar[x] = '\0';
-	return (ar);
+	return (a);
 }
