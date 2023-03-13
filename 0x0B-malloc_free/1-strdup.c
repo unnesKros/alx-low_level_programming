92fa33e668dbed080d8c137bfa0ac4a12f359afe
unnesKros <younesraf16@gmail.com>
2023-03-13 06:20:59 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - fonctionn that cp string to a new array
+ * @str: the string
+ * Return: new array
+ */
+
+
+char *_strdup(char *str)
+{
+	char *ar;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	ar = malloc(i * sizeof(char));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		ar[i] = str[i];
+	}
+
+	ar[i] = '\0';
+	return (ar);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,1 +1,3 @@
 char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
