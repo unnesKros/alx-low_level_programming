c3c260311fd42a8ba3f18c94bc8565b16ca6b227
unnesKros <younesraf16@gmail.com>
2023-03-13 05:54:22 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
+
 	ar = malloc(size * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		ar[i] = c;
 	return (ar);
