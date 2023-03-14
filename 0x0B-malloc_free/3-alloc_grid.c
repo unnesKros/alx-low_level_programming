4e429b799b986fb9ad5a3ece1b8c2ce73a2efcc3
unnesKros <younesraf16@gmail.com>
2023-03-14 06:27:32 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,33 +1,37 @@
 #include "main.h"
-#include "stdlib.h"
+#include <stdlib.h>
 
 /**
- * alloc_grid - function that t returns a pointer to a 2 dimensional array of
- * intgers.
- * @width: the width of the array
- * @height: the height of the array
- * Return: the array
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the array.
+ * @height: height of the array.
+ *
+ * Return: pointer of an array of integers
  */
+
 
 int **alloc_grid(int width, int height)
 {
-	int **ar;
 	int i, j;
+	int **arr;
 
-	if (width < 1 || height < 1)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ar = malloc(height * sizeof(int *));
 
-	if (ar == NULL)
+	arr = malloc(height * sizeof(int *));
+
+	if (arr == NULL)
 	{
-		free(ar);
+		free(arr);
 		return (NULL);
 	}
+
 	for (i = 0; i < height; i++)
 	{
-		ar[i] = malloc(width * sizeof(int));
-		if (ar[i] == NULL)
+		arr[i] = malloc(width * sizeof(int));
+
+		if (arr[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
 				free(arr[j]);
@@ -36,11 +40,11 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 
-		for (j = 0; j < width; j++)
-		{
-			ar[i][j] = 0;
-		}
 	}
 
-	return (ar);
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+
+	return (arr);
 }
