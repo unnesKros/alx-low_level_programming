bf0210b8981eb8113921a519b03b0245eab681a0
unnesKros <younesraf16@gmail.com>
2023-03-22 06:40:10 -0700

    task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array-iterator - function that print array ints with other fun
+ * @array: the input array
+ * @size: the array size
+ * @action: the function that use
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
