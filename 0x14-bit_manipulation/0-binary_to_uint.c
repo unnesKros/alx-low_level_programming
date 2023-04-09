49fe447cef32fec517f1709bab4c3c06a2a07099
unnesKros <younesraf16@gmail.com>
2023-04-09 15:55:26 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit -  function that converts a binary number to an unsigned int.
+ * binary_to_uint -  function that converts a binary number to an unsigned int.
  * @b: the binary string;
  * Return: return the unsigned int
  */
